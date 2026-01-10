2bf6d25a21ba7356c3bff8669ac903798f103fa2
tigattack <10629864+tigattack@users.noreply.github.com>
2026-01-10 22:59:03 +0000

    fix(util): add type hints

--- src/util.py
+++ src/util.py
@@ -2,13 +2,13 @@
 # Posted by jbaiter, modified by community. See post 'Timeline' for change history
 # Retrieved 2024-03-15, License - CC BY-SA 4.0
 # Further modified by tigattack
-def to_camel_case(snake_str):
+def to_camel_case(snake_str: str) -> str:
     """Convert snake_case string to camelCase"""
     camel_string = "".join(x.capitalize() for x in snake_str.lower().split("_"))
     return snake_str[0].lower() + camel_string[1:]
 
 
-def from_camel_case(camel_str):
+def from_camel_case(camel_str: str) -> str:
     """Convert camelCase string to snake_case"""
     result = []
     for i, char in enumerate(camel_str):

b1a97cec99eb48346861cb437b714f9d6a3ec133
tigattack <10629864+tigattack@users.noreply.github.com>
2026-01-10 20:41:11 +0000

    chore(lubelogger): add attribution for `to_camel_case()`

--- src/lubelogger.py
+++ src/lubelogger.py
@@ -6,6 +6,10 @@ from requests.auth import HTTPBasicAuth
 
 logger = logging.getLogger(__name__)
 
+# Source - https://stackoverflow.com/a/19053800
+# Posted by jbaiter, modified by community. See post 'Timeline' for change history
+# Retrieved 2024-03-15, License - CC BY-SA 4.0
+# Further modified by tigattack
 def to_camel_case(snake_str):
     """Convert snake_case string to camelCase"""
     camel_string = "".join(x.capitalize() for x in snake_str.lower().split("_"))
